ff2041f7d24f4c687a23fe1da4709b7475f8347b
naveedwani <waninaveed01@gmail.com>
2026-05-18 12:53:18 +0530

    Connect to PostgreSQL and update root route

--- app/server.js
+++ app/server.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const { Client } = require("pg");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("EKS GitOps Project Running");
+const client = new Client({
+  host: "postgres-postgresql",
+  user: "myuser",
+  password: "mypassword",
+  database: "mydb",
+  port: 5432,
+});
+
+client.connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch(err => console.error(err));
+
+app.get("/", async (req, res) => {
+  const result = await client.query("SELECT NOW()");
+  res.send(`Database Connected: ${result.rows[0].now}`);
 });
 
 app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+  console.log("Server running");
 });
